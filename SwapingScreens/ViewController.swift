4b4a00d8a8b2d00cbd6e4bfd6992c87c6334b435
Rong Lu <lrh@Rongs-iMac.local>
2016-09-30 03:40:58 -0400

    Initial Commit

---
+++ SwapingScreens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwapingScreens
+//
+//  Created by Rong Hui Lu on 2016-09-30.
+//  Copyright © 2016 Rong Hui Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
